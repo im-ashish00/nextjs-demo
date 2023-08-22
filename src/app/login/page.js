1d2ab99f35015c4fc5361cd0f142a6ef2573b533
Ashish Sharma <iamashishsharma00@gmail.com>
2023-08-22 14:59:55 +0530

    Added basic signup & login template with profile path

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,7 +1,46 @@
-export default function LoginPage() {
+"use client";
+import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { axios } from "axios";
+
+export default function LogininPage() {
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+  });
+
+  // Async function because we'll be talking to DB.
+  const onLogin = async () => {};
+
   return (
-    <>
-      <h1>Login Page 🔑</h1>
-    </>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>LOGIN 🔐</h1>
+      <br />
+      <hr />
+      <label htmlFor="email">Email</label>
+      <input
+        className="p-2 text-black border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        id="email"
+        type="email"
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+      />
+      <label htmlFor="password">Password</label>
+      <input
+        className="p-2 text-black border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        id="password"
+        type="password"
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+      />
+      <button
+        onClick={onLogin}
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+      >
+        Login here
+      </button>
+      <Link href="/signup">Don't have an account yet?</Link>
+    </div>
   );
 }
--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -1,7 +1,55 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { axios } from "axios";
+
 export default function SignUpPage() {
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+
+  // Async function because we'll be talking to DB.
+  const onSignup = async () => {};
+
   return (
-    <>
-      <h1>SignUp 🔐</h1>
-    </>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>SIGNUP 🔐</h1>
+      <br />
+      <hr />
+      <label htmlFor="username">Username</label>
+      <input
+        className="p-2 text-black border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        id="username"
+        type="text"
+        value={user.username}
+        onChange={(e) => setUser({ ...user, username: e.target.value })}
+      />
+      <label htmlFor="email">Email</label>
+      <input
+        className="p-2 text-black border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        id="email"
+        type="email"
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+      />
+      <label htmlFor="password">Password</label>
+      <input
+        className="p-2 text-black border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        id="password"
+        type="password"
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+      />
+      <button
+        onClick={onSignup}
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+      >
+        Signup here
+      </button>
+      <Link href="/login">Visit Login Page</Link>
+    </div>
   );
 }
